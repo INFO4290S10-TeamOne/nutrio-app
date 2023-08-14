a3ccf67942c951438ae4580d6f5a60cf8407b665
Gunn Hwang <gunhwang99@gmail.com>
2023-08-13 19:15:48 -0700

    Change styling

--- screens/ScaleScreen.tsx
+++ screens/ScaleScreen.tsx
@@ -1,38 +1,38 @@
-import { View, Text,StyleSheet } from 'react-native';
+import { Circle, VStack, HStack, Text } from 'native-base';
 
 const ScaleScrren = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View >
-     
-      <Text style={style.scale}>0.g</Text>
-      <Text style={style.items}>Select item</Text>
-      <Text style={style.items}>0.0 Cal</Text>
-      <Text style={style.items}>0.0g Protein</Text>
-      <Text style={style.items}>0.0g fat</Text>
-      </View>
-    </View>
+    <VStack space={8} alignItems={'center'} justifyItems={'center'} my={10}>
+      <Circle size='250px' bg='violet.300'>
+        <VStack space={4} alignItems={'center'}>
+          <Text fontSize={24}>Weight</Text>
+          <HStack space={4}>
+            <Text fontSize={52}>260</Text>
+            <Text fontSize={52}>g</Text>
+          </HStack>
+        </VStack>
+      </Circle>
+      <VStack alignItems={'center'} justifyContent={'space-between'}>
+        <Text fontSize={32}>Chicken Breast</Text>
+        <Text underline={true} fontSize={18} color='violet.500'>
+          Change item
+        </Text>
+      </VStack>
+      <VStack alignItems={'center'} justifyContent={'space-between'} w={'100%'}>
+        <HStack space={4} justifyContent={'space-between'} alignItems={'center'} w={'50%'}>
+          <Text fontSize={24}>263.3</Text>
+          <Text fontSize={24}> Calories</Text>
+        </HStack>
+        <HStack space={4} justifyContent={'space-between'} alignItems={'center'} w={'50%'}>
+          <Text fontSize={24}>9.1 g</Text>
+          <Text fontSize={24}>Protein</Text>
+        </HStack>
+        <HStack space={4} justifyContent={'space-between'} alignItems={'center'} w={'50%'}>
+          <Text fontSize={24}>22.1 g</Text>
+          <Text fontSize={24}>Fat</Text>
+        </HStack>
+      </VStack>
+    </VStack>
   );
 };
-const style = StyleSheet.create({
-  scale:{
-    fontWeight: 'bold',
-    fontSize: 20,
-    justifyContent:'center',
-    width: 150,
-    height: 150,
-    backgroundColor:'grey',
-    borderRadius: 150/2,
-    padding:50,
-    margin:20,
-  },
-  items:{
-   
-    fontSize: 16,
-    margin:20,
-
-
-  }
-})
 export default ScaleScrren;
-
